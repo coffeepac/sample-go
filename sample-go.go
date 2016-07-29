a3fb4983233eaedebcd9136fdb794efec7afd62c
Patrick Christopher <pat@Patricks-MacBook-Pro.local>
2016-07-29 16:02:11 -0700

    its whats for dinner

--- sample-go.go
+++ sample-go.go
@@ -6,9 +6,12 @@ import (
   http "net/http"
 )
 
+var HealthBadCount = 0
+
 func statusServer() {
     http.HandleFunc("/ping", pingHandle)
     http.HandleFunc("/health", healthHandle)
+    http.HandleFunc("/healthBad", healthBadHandle)
 
     //  create server that doesn't leave things open forever
     s := &http.Server{
@@ -25,6 +28,14 @@ func pingHandle(w http.ResponseWriter, r *http.Request){
 
 func healthHandle(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "health\n")
+}
+
+func healthBadHandle(w http.ResponseWriter, r *http.Request){
+    HealthBadCount += 1
+    if HealthBadCount > 15 {
+        w.WriteHeader(http.StatusInternalServerError)
+    }
+    fmt.Fprintf(w, "health count %d next\n", HealthBadCount)
 }
 
 func main() {
