dea007c466404afde580528b08ef680ca08fa41b
Patrick Christopher <coffeepac@gmail.com>
2016-08-18 16:49:16 -0700

    add some more handlers, futz with PVs

--- sample-go.go
+++ sample-go.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+  "os"
   "fmt"
   "time"
+  ioutil "io/ioutil"
   http "net/http"
 )
 
+// HealthBadCount suck it
 var HealthBadCount = 0
 
 func statusServer() {
     http.HandleFunc("/ping", pingHandle)
     http.HandleFunc("/health", healthHandle)
     http.HandleFunc("/healthBad", healthBadHandle)
-    http.HandleFunc("/pvData", pvDataHandle)
+    http.HandleFunc("/pvDataReturn", pvDataReturn)
+    http.HandleFunc("/pvDataSet", pvDataSet)
 
     //  create server that doesn't leave things open forever
     s := &http.Server{
@@ -24,23 +28,55 @@ func statusServer() {
 }
 
 func pingHandle(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "PONG\n")
+    if r.Method == "GET" {
+        fmt.Fprintf(w, "PONG\n")
+    }
 }
 
 func healthHandle(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "health\n")
+    if r.Method == "GET" {
+        fmt.Fprintf(w, "health\n")
+    }
 }
 
-func pvDataHandle(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "blob\n")
+func pvDataReturn(w http.ResponseWriter, r *http.Request){
+    if r.Method == "GET" {
+        pvData, err := ioutil.ReadFile("/mnt/cephfs/pvData")
+        if err != nil {
+            fmt.Fprintf(w, "PV not present\n")
+        } else {
+            fmt.Fprintf(w, string(pvData[:]) + "\n")
+        }
+    }
+}
+
+func pvDataSet(w http.ResponseWriter, r *http.Request){
+    if r.Method == "PUT" {
+        bytes := make([]byte, 100)
+        _, err := r.Body.Read(bytes)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)            
+            fmt.Fprintf(w, "Failed to read PUT data\n")
+        }
+        
+        err = ioutil.WriteFile("/mnt/cephfs/pvData", bytes, os.ModePerm)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            fmt.Fprintf(w, "PV not present\n")
+        } else {
+            fmt.Fprintf(w, "Data saved")
+        }
+    }
 }
 
 func healthBadHandle(w http.ResponseWriter, r *http.Request){
-    HealthBadCount += 1
-    if HealthBadCount > 15 {
-        w.WriteHeader(http.StatusInternalServerError)
+    if r.Method == "GET" {
+        HealthBadCount++
+        if HealthBadCount > 15 {
+            w.WriteHeader(http.StatusInternalServerError)
+        }
+        fmt.Fprintf(w, "health count %d next\n", HealthBadCount)
     }
-    fmt.Fprintf(w, "health count %d next\n", HealthBadCount)
 }
 
 func main() {
